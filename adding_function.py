46b2c782835627a67ec58833e9bf57b785f8baf9
Jarrett L. Joubert <118229473+JJ-rue@users.noreply.github.com>
2023-03-30 18:19:19 -0700

    added comments to the function

--- adding_function.py
+++ adding_function.py
@@ -1,6 +1,8 @@
+# The below function describes simple addition.
 def add_numbers(num1, num2):
     sum = num1 + num2
     print('Sum: ',sum)
 
+# input two numbers (X, Y) to get the sum
 add_numbers(5, 4)
 
